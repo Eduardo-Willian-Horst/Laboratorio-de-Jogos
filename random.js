c0083a61374414da980f9e2ba822a6bd9b3f141e
Eduardo-Willian-Horst <133250122+Eduardo-Willian-Horst@users.noreply.github.com>
2023-06-21 13:46:15 -0300

    Add files via upload

---
+++ random.js
@@ -0,0 +1,7 @@
+const listaPaginas = ['a_lenda_do_heroi.html', 'gow.html', 'samorost.html'];
+
+let game = listaPaginas[Math.floor(Math.random() * listaPaginas.length)];
+
+console.log(game);
+
+document.getElementById('botao-random').innerHTML = '<button class="botao-random" onclick="window.location.href= '+ "'" + game + "'" + '">Surpreenda-me...</button>';
